9c04b7ba69a355b9f037fbca37846962ebbba4fe
Joel Campos <joelcamposoliva9000@gmail.com>
2023-09-10 20:45:35 +0200

    More style

--- src/app.tsx
+++ src/app.tsx
@@ -2,15 +2,25 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./router/router";
 import routes_definition from "./router/routes-definition";
 import { BrowserRouter } from "react-router-dom";
+import { ConfigProvider } from "antd";
+import { purple } from "@ant-design/colors";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Router routes={routes_definition} />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: purple[5],
+        },
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Router routes={routes_definition} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ConfigProvider>
   );
 }
--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -1,18 +1,25 @@
-import { Col, Row, Typography } from "antd";
+import { Col, Row, Typography, theme } from "antd";
 import UsernameAndPasswordFormStep from "./login-steps/username-and-password-form-step";
 import tmw_universe_logo from "../../assets/branding/tmw_universe_logo.png";
 import styles from "./login-form.module.css";
 import { useTranslation } from "react-i18next";
 import { Translations } from "../../i18n/translations.enum";
 
-const { Title } = Typography;
+const { Title, Link } = Typography;
 
 type Props = {
   domain: string;
 };
 
 export default function LoginForm({ domain }: Props) {
+  const { useToken } = theme;
+  const { token } = useToken();
+
   const { t } = useTranslation([Translations.login]);
+
+  const openDomain = () => {
+    window.open("https://" + domain, "_blank");
+  };
 
   return (
     <Row gutter={[12, 48]}>
@@ -21,10 +28,11 @@ export default function LoginForm({ domain }: Props) {
           <Col span={24} className={styles.logo}>
             <img src={tmw_universe_logo} alt="TMW Universe logo" />
           </Col>
-          <Col span={24}>
-            <Title level={4} className={styles.title}>
-              {t("form.Title")}
-            </Title>
+          <Col span={24} className={styles["title-container"]}>
+            <Title level={4}>{t("form.Title")}</Title>
+            <Link onClick={openDomain} style={{ color: token.colorPrimary }}>
+              {domain}
+            </Link>
           </Col>
         </Row>
       </Col>
